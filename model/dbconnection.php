1371e70c267aeb570b3651c6c7f7032cd6f7daa1
NPDelMundo <delmundonielpatrick@gmail.com>
2024-10-21 02:08:51 +0800

    con

--- model/dbconnection.php
+++ model/dbconnection.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-// $con = new mysqli("localhost", "u789905971_herbert", "REyes0302", "u789905971_fepBsu");
-$con = new mysqli("localhost", "root", "", "super-final-fep-bsu");
-=======
+
 $con = new mysqli("localhost", "u789905971_herbert", "REyes0302", "u789905971_fepBsu");
->>>>>>> 31f8a70088bc05d0d61a0c45627f98de73d1fda7
+// $con = new mysqli("localhost", "root", "", "super-final-fep-bsu");
 
 if (!$con) {
     die(mysqli_error($con));
